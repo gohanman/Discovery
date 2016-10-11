2e0c1bfa53c6c376a2261f55d981794725095210
Andy Theuninck <andy@gohanman.com>
2016-10-11 10:41:03 -0500

    Init repo

---
+++ Discovery.Tests/Discovery.Tests.fs
@@ -0,0 +1,35 @@
+﻿namespace DiscoveryTests
+
+open Discovery
+open NUnit.Framework
+open FsUnit
+
+module Tests =
+
+    let isNone value =
+        match value with
+        | Some s -> false
+        | None -> true
+
+    [<Test>]
+    let ``Find Class`` () =
+        isNone (Discovery.getType "System.Type") |> should equal false
+
+    [<Test>]
+    let ``Do Not Find Class`` () =
+        (isNone (Discovery.getType "System.Nonsense")) |> should equal true
+
+    [<Test>]
+    let ``Find Subclasses`` () =
+        let matches = Discovery.getSubclasses "System.Type"
+        matches.IsEmpty |> should equal false
+
+    [<Test>]
+    let ``Do Not Find Subclasses`` () =
+        let matches = Discovery.getSubclasses "System.Nonsense"
+        matches.IsEmpty |> should equal true
+
+    [<Test>]
+    let ``Do Not Find Interfaces`` () =
+        let matches = Discovery.getImplementations "System.Nonsense"
+        matches.IsEmpty |> should equal true
---
+++ Discovery/Discovery.fs
@@ -0,0 +1,33 @@
+﻿namespace Discovery
+
+open System
+
+module Discovery =
+    
+    let assemblies = AppDomain.CurrentDomain.GetAssemblies()
+    let types = assemblies 
+                    |> Array.toList
+                    |> List.map (fun i -> i.GetTypes())
+                    |> List.map (fun i -> Array.toList i)
+                    |> List.concat
+
+    let getType (name:string) =
+        types |> List.tryFind (fun i -> i.FullName = name)
+
+    let getSubclasses (name:string) =
+        let actualType = getType name
+        match actualType with
+        | None -> []
+        | Some t -> types |> List.filter (fun i -> i.IsSubclassOf(t))
+
+    let implements (someType:Type) (iface:Type) =
+        someType.GetInterfaces()
+        |> Array.toList
+        |> List.exists (fun i -> i = iface)
+
+    let getImplementations (name:string) =
+        let actualType = getType name
+        match actualType with
+        | None -> []
+        | Some t -> types |> List.filter (fun i -> implements i t)
+
